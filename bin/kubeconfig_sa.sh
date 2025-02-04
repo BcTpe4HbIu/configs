fb0bee9372875d0239e9ff21b4e116a2244113a4
Igor Golubkov <bctpe4hbiu@yandex.ru>
2025-02-04 23:49:49 +0300

    feat: update kubeconfig_sa for new k8s

--- bin/kubeconfig_sa.sh
+++ bin/kubeconfig_sa.sh
@@ -1,38 +1,44 @@
-#!/bin/bash -e
+#!/bin/bash
 
-# Usage ./k8s-service-account-kubeconfig.sh ( namespace ) ( service account name )
+set -euxo pipefail
+
 
 TEMPDIR=$( mktemp -d )
 
 trap "{ rm -rf $TEMPDIR ; exit 255; }" EXIT
 
-SA_SECRET=$( kubectl get sa -n $1 $2 -o jsonpath='{.secrets[0].name}' )
+NAMESPACE=$1
+SECRET=$2
+OUTPUT=$3
+
+if [ -z "$NAMESPACE" -o -z "$SECRET" -o -z "$OUTPUT" ] ; then
+    echo usage $0 namespace secret output
+    exit 1
+fi
 
 # Pull the bearer token and cluster CA from the service account secret.
-BEARER_TOKEN=$( kubectl get secrets -n $1 $SA_SECRET -o jsonpath='{.data.token}' | base64 -d )
-kubectl get secrets -n $1 $SA_SECRET -o jsonpath='{.data.ca\.crt}' | base64 -d > $TEMPDIR/ca.crt
+BEARER_TOKEN=$( kubectl get secrets -n $NAMESPACE $SECRET -o jsonpath='{.data.token}' | base64 -d )
+kubectl get secrets -n $NAMESPACE $SECRET -o jsonpath='{.data.ca\.crt}' | base64 -d > $TEMPDIR/ca.crt
 
 CLUSTER_URL=$( kubectl config view --minify --flatten -o jsonpath='{.clusters[0].cluster.server}' )
 
 
-KUBECONFIG=kubeconfig
-
-kubectl config --kubeconfig=$KUBECONFIG \
+kubectl config --kubeconfig=$OUTPUT \
     set-cluster \
     $CLUSTER_URL \
     --server=$CLUSTER_URL \
     --certificate-authority=$TEMPDIR/ca.crt \
     --embed-certs=true
 
-kubectl config --kubeconfig=$KUBECONFIG \
-    set-credentials $1-$2 --token=$BEARER_TOKEN
+kubectl config --kubeconfig=$OUTPUT \
+    set-credentials $NAMESPACE-$SECRET --token=$BEARER_TOKEN
 
-kubectl config --kubeconfig=$KUBECONFIG \
-    set-context $1-$2 \
+kubectl config --kubeconfig=$OUTPUT \
+    set-context $NAMESPACE-$SECRET \
     --cluster=$CLUSTER_URL \
-    --user=$1-$2
+    --user=$NAMESPACE-$SECRET
 
-kubectl config --kubeconfig=$KUBECONFIG \
-    use-context $1-$2
+kubectl config --kubeconfig=$OUTPUT \
+    use-context $NAMESPACE-$SECRET
 
-echo "kubeconfig written to file \"$KUBECONFIG\""
+echo "kubeconfig written to file \"$OUTPUT\""
